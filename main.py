667253f065cd94b455740d0585a7865c735467f0
FloFlo <florentraimond@hotmail.com>
2025-05-11 13:27:06 +0200

    11/05/2025 save1

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def get_book_text (filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
+
 def main ():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    number_words(text)
+    
+from stats import number_words
+
 main ()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def number_words(text):
+    words = text.split()
+    total_words = len(words)
+    print (total_words, f"words found in the document")
