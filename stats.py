3b14485c5c1d5b4046660822901134f4450239d3
FloFlo <florentraimond@hotmail.com>
2025-05-15 21:15:52 +0200

    15/05 save1

--- stats.py
+++ stats.py
@@ -1,13 +1,26 @@
-def number_words(text):
+def get_num_words(text):
     words = text.split()
-    total_words = len(words)
-    print (total_words, f"words found in the document")
+    return len(words)
 
-def number_characters(text):
-    list_characters = []
-    for i in text:
-        character = i.lower()
-        list_characters.append(character)
-        #print(list_characters)
-    dictionnary ={item: list_characters.count(item) for item in set(list_characters)}
-    print(dictionnary)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
